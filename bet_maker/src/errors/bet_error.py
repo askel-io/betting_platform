a1e15b81f7f4bd66967bf2fcd6e13861881eb809
Alex <askelio.official@gmail.com>
2026-07-14 17:55:31 +0200

    Add event not found and not avaiable errors

--- bet_maker/src/errors/bet_error.py
+++ bet_maker/src/errors/bet_error.py
@@ -13,3 +13,15 @@ class BetNotFoundError(DomainError):
     def __init__(self, bet_id: str) -> None:
         self.bet_id = bet_id
         super().__init__(f"Bet not found: {bet_id}")
+
+
+class EventNotFoundError(DomainError):
+    def __init__(self, event_id: str) -> None:
+        self.event_id = event_id
+        super().__init__(f"Event not found: {event_id}")
+
+
+class EventNotAvailableError(DomainError):
+    def __init__(self, event_id: str) -> None:
+        self.event_id = event_id
+        super().__init__(f"Event is not available for betting: {event_id}")
