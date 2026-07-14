7478dfb542ccdae3ebd948843708589d0193719b
Alex <askelio.official@gmail.com>
2026-07-14 14:23:53 +0200

    Add event router

--- line_provider/src/presentation/rest/api/v1/router.py
+++ line_provider/src/presentation/rest/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from line_provider.src.presentation.rest.api.v1.events import router as events_router
 from line_provider.src.presentation.rest.api.v1.health import router as health_router
 
 router = APIRouter()
 router.include_router(health_router)
+router.include_router(events_router)
