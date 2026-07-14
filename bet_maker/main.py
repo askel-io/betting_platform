9f9d668f1dd7bc23c62f3cfc310bee2950c194f5
Alex <askelio.official@gmail.com>
2026-07-14 17:06:48 +0200

    Register exception handlers

--- bet_maker/main.py
+++ bet_maker/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from bet_maker.src.presentation.rest.api.v1.router import router as v1_router
+from bet_maker.src.presentation.rest.exception_handlers import register_exception_handlers
 
 app = FastAPI(title="Bet Maker", version="0.1.0")
 app.include_router(v1_router, prefix="/rest/api/v1")
+register_exception_handlers(app)
